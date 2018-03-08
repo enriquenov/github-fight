021a631762c0a7bc8b580f56b6c582f47499e6ba
enrique novoa <luisenriquenovoa@gmail.com>
2018-03-07 20:48:05 -0800

    2nd

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,14 @@ module.exports = {
   output:  {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
-  }
+  },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader']},
+      { test: /\.css$/, use: ['style-loader', 'css-loader']}
     ]
   },
-  plugins = [new HtmlWebpackPlugin({
+  plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
   })]
 }
