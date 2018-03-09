57a347a551af10f8185ca2ce6465eaafe19d9f78
enrique novoa <luisenriquenovoa@gmail.com>
2018-03-08 21:46:14 -0800

    5th

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,10 +1,39 @@
 var React = require('react');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function(){
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render() {
+    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"]
     return (
       <div>
-        Popular!
+        <p>Selected Language: {this.state.selectedLanguage}</p>
+        <ul className="languages">
+          {languages.map((lang) => {
+            return (
+              <li
+                onClick={this.updateLanguage.bind(null, lang)}
+                key={lang}>
+                {lang}
+              </li>
+            )
+          }, this)}
+        </ul>
       </div>
     )
   }
