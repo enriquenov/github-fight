263805a3b39b9e5c811c6ac7d23cee44137dbb27
enrique novoa <luisenriquenovoa@gmail.com>
2018-03-08 21:06:36 -0800

    4th

--- app/index.js
+++ app/index.js
@@ -1,20 +1,13 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 require('./index.css');
+var App = require('./components/App');
 
 // STATE
 // LIFECYCLE EVENT
 // UI
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
+
 
 ReactDom.render(
   <App />, document.getElementById('app')
