e4726e25f0b6d110e31aaf850083c11086affe69
enrique novoa <luisenriquenovoa@gmail.com>
2018-03-14 23:05:28 -0700

    18th

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -8,7 +8,7 @@ function PlayerPreview(props) {
         <img
           className="avatar"
           src={props.avatar}
-          alt={'Avatar for ' props.username}
+          alt={'Avatar for ' + props.username}
         />
         <h2 className="username">@{props.username}</h2>
         <button
@@ -102,6 +102,7 @@ class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleSubmit(id, username) {
@@ -114,9 +115,20 @@ class Battle extends React.Component {
     });
   }
 
+  handleReset(id) {
+    this.setState(function(){
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
+      return newState;
+    });
+  }
+
   render() {
     var playerOneName = this.state.playerOneName;
     var playerTwoName = this.state.playerTwoName;
+    var playerOneImage = this.state.playerOneImage;
+    var playerTwoImage = this.state.playerTwoImage;
 
     return (
       <div>
@@ -127,11 +139,25 @@ class Battle extends React.Component {
             label="Player One"
             onSubmit={this.handleSubmit} />}
 
+          {playerOneImage !== null &&
+          <PlayerPreview
+            avatar={playerOneImage}
+            username={playerOneName}
+            onReset={this.handleReset}
+            id="playerOne"/>}
+
           {!playerTwoName &&
           <PlayerInput
             id="playerTwo"
             label="Player Two"
             onSubmit={this.handleSubmit} />}
+
+          {playerTwoImage !== null &&
+          <PlayerPreview
+            avatar={playerTwoImage}
+            username={playerTwoName}
+            onReset={this.handleReset}
+            id="playerTwo"/>}
         </div>
       </div>
     )
