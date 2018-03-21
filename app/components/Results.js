0474f861a43af2fdabfc3b82118ce51c5b131f31
enrique novoa <luisenriquenovoa@gmail.com>
2018-03-20 22:26:42 -0700

    25th

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,20 @@
 var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/api');
 
 class Results extends React.Component {
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function(results){
+      console.log(results);
+    });
+  }
   render() {
+
+    console.log(players);
     return (
       <div>
         <span>Results!</span>
